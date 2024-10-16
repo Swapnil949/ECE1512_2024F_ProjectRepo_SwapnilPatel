bd83cb30e35a8f352c57d3f70e6b4291cc11fe52
Swapnil Patel <swapnil949@outlook.com>
2024-10-16 00:44:48 -0400

    updated database script and added basic dataset reading capability

--- project_A/mhist_dataset/split_train_test.py
+++ project_A/mhist_dataset/split_train_test.py
@@ -9,26 +9,27 @@ def get_split_data() -> pd.DataFrame:
     df_train = df.query('Partition == "train"')
     df_test = df.query('Partition == "test"')
 
-    df = pd.concat([df_train, df_test])
-    assert not df.duplicated().any()
-    return df
-
+    return df_train, df_test
 
 def main():
     split_root = "images-split"
     Path(split_root).mkdir(exist_ok=True)
 
     def make_symlink(row):
-        src = Path("images") / row["Image Name"]
+        # get current directory
+        src = Path.cwd() / Path("images") / row["Image Name"]
         label = row["Majority Vote Label"]
         partition = row["Partition"]
         dst = Path(split_root) / partition / label / row["Image Name"]
         dst.parent.mkdir(parents=True, exist_ok=True)
         os.symlink(src, dst)
 
-    df = get_split_data()
-    df.apply(make_symlink, axis=1)
+    df_train, df_test = get_split_data()
+    df_train.to_csv("train_annotations.csv", index=False)
+    df_test.to_csv("test_annotations.csv", index=False)
 
+    df_train.apply(make_symlink, axis=1)
+    df_test.apply(make_symlink, axis=1)
 
 if __name__ == "__main__":
     main()
