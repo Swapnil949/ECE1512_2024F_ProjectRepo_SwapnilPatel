5660a137520c8bb1b515cfee27c1e9c49e162d3b
Swapnil Patel <swapnil949@outlook.com>
2024-10-20 00:05:46 -0400

    added work in progress for attention matching algorithm

--- project_A/src/utils.py
+++ project_A/src/utils.py
@@ -9,6 +9,49 @@ from torchvision import datasets, transforms
 from scipy.ndimage import rotate as scipyrotate
 from networks import MLP, ConvNet, LeNet, AlexNet, AlexNetBN, VGG11, VGG11BN, ResNet18, ResNet18BN_AP, ResNet18BN
 
+
+"""
+Attention module is taken from the following repository:
+
+https://github.com/DataDistillation/DataDAM/blob/main/utils.py
+
+A. Sajedi, S. Khaki, E. Amjadian, L. Z. Liu, Y. A. Lawryshyn, and K. N. Plataniotis, DataDAM: Efficient Dataset Distillation with Attention Matching. 2023. [Online]. Available: https://arxiv.org/abs/2310.00093 
+"""
+def get_attention(feature_set, param=0, exp=4, norm='l2'):
+    if param==0:
+        attention_map = torch.sum(torch.abs(feature_set), dim=1)
+    
+    elif param ==1:
+        attention_map =  torch.sum(torch.abs(feature_set)**exp, dim=1)
+    
+    elif param == 2:
+        attention_map =  torch.max(torch.abs(feature_set)**exp, dim=1)
+       
+    if norm == 'l2': 
+        # Dimension: [B x (H*W)] -- Vectorized
+        vectorized_attention_map =  attention_map.view(feature_set.size(0), -1)
+        normalized_attention_maps = F.normalize(vectorized_attention_map, p=2.0)
+    
+    elif norm == 'fro':
+        # Dimension: [B x H x W] -- Un-Vectorized
+        un_vectorized_attention_map =  attention_map
+        # Dimension: [B]
+        fro_norm = torch.sum(torch.sum(torch.abs(attention_map)**2, dim=1), dim=1)
+        # Dimension: [B x H x W] -- Un-Vectorized)
+        normalized_attention_maps = un_vectorized_attention_map / fro_norm.unsqueeze(dim=-1).unsqueeze(dim=-1)
+    elif norm == 'l1': 
+        # Dimension: [B x (H*W)] -- Vectorized
+        vectorized_attention_map =  attention_map.view(feature_set.size(0), -1)
+        normalized_attention_maps = F.normalize(vectorized_attention_map, p=1.0)
+    
+    elif norm =='none':
+        normalized_attention_maps = attention_map
+        
+    elif norm == 'none-vectorized':
+        normalized_attention_maps =  attention_map.view(feature_set.size(0), -1)
+    
+    return normalized_attention_maps
+
 def get_dataset(dataset, data_path):
     if dataset == 'MNIST':
         channel = 1
@@ -98,7 +141,8 @@ def get_dataset(dataset, data_path):
 
 
     testloader = torch.utils.data.DataLoader(dst_test, batch_size=256, shuffle=False, num_workers=0)
-    return channel, im_size, num_classes, class_names, mean, std, dst_train, dst_test, testloader
+    trainloader = torch.utils.data.DataLoader(dst_train, batch_size=256, shuffle=True, num_workers=0)
+    return channel, im_size, num_classes, class_names, mean, std, dst_train, dst_test, testloader, trainloader
 
 
 
